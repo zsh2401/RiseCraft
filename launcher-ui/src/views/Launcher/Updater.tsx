c9538e7260e51d00a4f40241e1a27a3a6c63a81a
zsh2401 <zsh2401@163.com>
2024-07-18 06:12:32 +0800

    fixed an error

--- launcher-ui/src/views/Launcher/Updater.tsx
+++ launcher-ui/src/views/Launcher/Updater.tsx
@@ -6,8 +6,17 @@ export function Updater(props: {
     // const [stage,setStage] = useState<"checking" | "need-upgrade">("checking")
     useEffect(() => {
         (async () => {
-            const should = await window.RiseCraft.isUpgradable()
-            await window.RiseCraft.performUpgrade()
+            try{
+                const should = await window.RiseCraft.isUpgradable()
+                if(should){
+                    await window.RiseCraft.performUpgrade()
+                }else{
+                    props.onNoUpdate()
+                }
+            }catch{
+                props.onNoUpdate()
+            }
+          
         })()
     }, [props])
     return <div className="updater">
