fb9f80d0e8f6417d5e7611a63465c8f55116f5fc
zsh2401 <zsh2401@163.com>
2024-07-18 04:18:31 +0800

    done

--- rise-craft-desktop/main.py
+++ rise-craft-desktop/main.py
@@ -5,7 +5,7 @@ if len(sys.argv) > 1 and sys.argv[1] == "update":
     update.do_update(sys.argv[2],"https://api.mc.zsh2401.top")
 elif len(sys.argv) > 1 and sys.argv[1] == "server":
     import server
-    server.support_server(os.path.dirname(os.path.abspath(sys.argv[0])))
+    server.support_server(os.path.dirname(os.path.abspath(sys.argv[0])),os.path.abspath(sys.argv[2]))
 else:
     import client.launcher
     client.launcher.run("https://api.mc.zsh2401.top",os.path.dirname(os.path.abspath(sys.argv[0])))
--- rise-craft-desktop/server/__init__.py
+++ rise-craft-desktop/server/__init__.py
@@ -3,36 +3,37 @@ from shared.version_engine.local import read_local
 
 import os
 import json
-def support_server(dir:str):
+def support_server(rootDir:str,webDir):
     from flask import Flask
     app = Flask(__name__)
 
     @app.route('/<path:path>')
     def serve_static(path):
+        print(webDir)
         try:
-            return send_from_directory(os.path.join(os.getcwd(),dir,"web"), path)
+            return send_from_directory(webDir, path)
         except:
-            return send_from_directory(os.path.join(os.getcwd(),dir,"web"), "index.html")
+            return send_from_directory(webDir, "index.html")
         
     @app.route('/')
     def index():
-        return send_from_directory(os.path.join(os.getcwd(),dir,"web"), "index.html")
+        return send_from_directory(webDir, "index.html")
     
     @app.route('/api/version')
     def version():
-        with open(os.path.join(dir,"version.json")) as f:
+        with open(os.path.join(rootDir,"version.json")) as f:
             return json.loads(f.read())
     
     @app.route('/api/full-version')
     def full_version():
-        return read_local(dir)
+        return read_local(rootDir)
     
     @app.route('/api/download/<path:subpath>')
     def download_file(subpath):
         print(os.getcwd())
-        directory = os.path.join(os.getcwd(),dir, subpath)
+        directory = os.path.join(os.getcwd(),rootDir, subpath)
         return send_from_directory(os.path.dirname(directory),os.path.basename(directory))
     
  
     
-    app.run(debug=True,port=9999)
+    app.run(port=9999)
