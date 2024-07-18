9689e3a6be47915b7939a0f6a1587234ff4e0050
zsh2401 <zsh2401@163.com>
2024-07-18 21:50:17 +0800

    new server usage

--- rise-craft-desktop/main.py
+++ rise-craft-desktop/main.py
@@ -6,7 +6,7 @@ if len(sys.argv) > 1 and sys.argv[1] == "update":
     update.gui(sys.argv[2],"http://localhost:5173/upgrading","https://api.mc.zsh2401.top")
 elif len(sys.argv) > 1 and sys.argv[1] == "server":
     import server
-    server.support_server(os.path.dirname(os.path.abspath(sys.argv[0])),os.path.abspath(sys.argv[2]))
+    server.support_server(os.path.abspath(sys.argv[2]),os.path.abspath(sys.argv[3]))
 else:
     import client.launcher
     # client.launcher.run("https://api.mc.zsh2401.top",os.path.dirname(os.path.abspath(sys.argv[0])))
