0f49e0c2f34450de626587048a38618c53d07e96
zsh2401 <zsh2401@163.com>
2024-07-19 00:25:05 +0800

    better makefile

--- rise-craft-desktop/main.py
+++ rise-craft-desktop/main.py
@@ -2,13 +2,21 @@ import sys
 import os
 import update
 import server
+from shared.version_engine.local import read_local
+import json
+import sys
 import client.launcher
 if len(sys.argv) > 1 and sys.argv[1] == "update":
-
     update.gui(sys.argv[2],"https://api.mc.zsh2401.top/upgrading","https://api.mc.zsh2401.top")
     # update.gui(sys.argv[2],"http://localhost:5173/upgrading","https://api.mc.zsh2401.top")
 elif len(sys.argv) > 1 and sys.argv[1] == "server":
     server.support_server(os.path.abspath(sys.argv[2]),os.path.abspath(sys.argv[3]))
+elif len(sys.argv) > 1 and sys.argv[1] == "generate":
+    # game dir like .
+    # prefix like "https://risecraft.oss-rg-china-mainland.aliyuncs.com/RiseCraft/"
+    data = read_local(sys.argv[2],True,sys.argv[3])
+    with open("full-version.json","w") as f:
+        json.dump(data,f)
 else:
     # client.launcher.run("https://api.mc.zsh2401.top",os.path.dirname(os.path.abspath(sys.argv[0])))
     client.launcher.run("https://api.mc.zsh2401.top",str(os.path.dirname(os.path.abspath(sys.argv[0]))),"https://api.mc.zsh2401.top")
--- rise-craft-desktop/make-full-version.py
+++
@@ -1,6 +0,0 @@
-from shared.version_engine.local import read_local
-import json
-import sys
-data =  read_local(sys.argv[1],True,"https://risecraft.oss-rg-china-mainland.aliyuncs.com/RiseCraft/")
-with open("full-version.json","w") as f:
-    json.dump(data,f)
