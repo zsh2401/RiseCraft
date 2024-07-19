4b33700ae4b16ef2f2897f965900c1aaf59242ea
zsh2401 <zsh2401@163.com>
2024-07-20 01:33:25 +0800

    update server

--- rise-craft-desktop/server/__init__.py
+++ rise-craft-desktop/server/__init__.py
@@ -3,12 +3,23 @@ from shared.version_engine.local import read_local
 
 import os
 import json
-def support_server(fullVersionFile:str,webDir):
-    with open(fullVersionFile,"r") as f:
-        fullVersion = json.load(f)
-        
+import time
+import requests
+
+
+def support_server(oss:str,webDir):
     from flask import Flask
     app = Flask(__name__)
+    
+    lastTime = 0
+    data = None
+    def get_full_version():
+        if (data is None) or ((time.time() - lastTime) > 60):
+            print("refresh full version cache")
+            lastTime = time.time()
+            resp = requests.get(oss)
+            data = resp.json()
+        return data
 
     @app.route('/<path:path>')
     def serve_static(path):
@@ -25,13 +36,13 @@ def support_server(fullVersionFile:str,webDir):
     @app.route('/api/version')
     def version():
         return {
-            "code":fullVersion["code"],
-            "name":fullVersion["name"]
+            "code":get_full_version()["code"],
+            "name":get_full_version()["name"]
         }
     
     @app.route('/api/full-version')
     def full_version():
-        return fullVersion
+        return get_full_version()
         # return read_local(rootDir,True,"https://risecraft.oss-rg-china-mainland.aliyuncs.com/RiseCraft/")
     
     # @app.route('/api/download/<path:subpath>')
