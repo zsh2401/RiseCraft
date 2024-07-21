cf7c929801e874031afeff7e0e1cf78f3153d9c2
zsh2401 <zsh2401@163.com>
2024-07-21 16:29:16 +0800

    to fuck server

--- launcher-ui/src/views/Launcher/MainPane.tsx
+++ launcher-ui/src/views/Launcher/MainPane.tsx
@@ -29,8 +29,8 @@ export function Content() {
                 java,
                 versionName: "1.20.1-47.3.0",
                 userName,
-                server:"pi.mc",
-                port:2401,
+                server:"110.42.38.188",
+                port:25565,
                 jvmArguments: ["-Xmx4096m"],
                 baseVersionName: "1.20.1",
                 resolutionHeight: 720,
@@ -58,14 +58,12 @@ export function Content() {
     return <div className='content'>
         <h1>RiseCraft X</h1>
         <h3>Have fun!</h3>
-        <a target="_blank" href='https://baidu.com'>WOw</a>
         <div className='pad'>
             <Input value={userName} onChange={e => setUserName(e.target.value)} placeholder='用户名' />
             <br /><br />
             <Button size='large' danger onClick={kill}>关闭</Button>
             <Button loading={launching} size='large' type='primary' onClick={launch}>启动</Button>
         </div>
-        {navigator.userAgent}
         {
             (launching || failed) && <Status successLaunched={launched} />
         }
