bdd67f55f104880662c7957886021d555f3acbea
zsh2401 <zsh2401@163.com>
2024-07-24 01:31:15 +0800

    fixed upgrader

--- launcher-ui/src/views/Upgrading/index.tsx
+++ launcher-ui/src/views/Upgrading/index.tsx
@@ -1,7 +1,9 @@
 import { useCallback, useEffect, useState } from "react"
 import { UpdateProgressEvent } from "../../risecraft"
+import { prepareForBridge } from "../../Bridge"
 
 export function Upgrading() {
+    const [prepared, setPrepared] = useState(false)
     const [progress, setProgress] = useState<UpdateProgressEvent>({
         total: 0,
         step: "准备更新",
@@ -10,11 +12,22 @@ export function Upgrading() {
     const cb = useCallback((e: UpdateProgressEvent) => {
         setProgress(e)
     }, [setProgress])
+    
     useEffect(() => {
+        (async () => {
+            await prepareForBridge()
+            setPrepared(true)
+        })()
+    }, [setPrepared])
+
+    useEffect(() => {
+        if (!prepared) {
+            return
+        }
         window.RiseCraftFn.onUpgrading = cb
         return () => {
             delete window.RiseCraftFn.onUpgrading
         }
-    }, [cb])
+    }, [cb, prepared])
     return <div>{(progress.percent * 100).toFixed(2)}% {progress.step}</div>
 }
