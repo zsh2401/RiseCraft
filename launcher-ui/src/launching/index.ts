920f0a78c245972a9e15168fe81f8434b97031c4
zsh2401 <zsh2401@163.com>
2024-07-24 01:15:54 +0800

    no longer remove healtbar

--- launcher-ui/src/launching/index.ts
+++ launcher-ui/src/launching/index.ts
@@ -5,7 +5,7 @@ async function loadRegistry():Promise<TaskBase[]>{
     const REGISTRY: TaskBase[] = [
         //1.0.2->1.0.3
         await OneFileRemover.createForMod("旅行地图journeymap-1.20.1-5.10.1-forge.jar"),
-        await OneFileRemover.createForMod("mobhealthbar-forge-1.20.x-2.3.0.jar"),
+        // await OneFileRemover.createForMod("mobhealthbar-forge-1.20.x-2.3.0.jar"),
     ]
     return REGISTRY
 }
